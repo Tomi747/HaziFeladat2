c0f1ba58a2f81b2f73a7dbd25dcff65a7cb4b819
Tomi747 <36446657+Tomi747@users.noreply.github.com>
2018-03-08 17:57:58 +0100

    5,8,9,10,11,12 feladat és 15-ik félkészen

--- Feladat2.py
+++ Feladat2.py
@@ -58,7 +58,7 @@ def feladat_5(n):
     max_oszto=0
     for i in range(1,n+1):
         oszto_db=1
-        for j in range(1,int(i/2)+1):
+        for j in range(1,n+1):
             if i%j==0:
                 oszto_db=oszto_db+1
         if oszto_db>max_oszto:
@@ -66,6 +66,80 @@ def feladat_5(n):
             max_oszto=oszto_db
     print(eredmeny)
 
+def feladat_8(n):
+    s=1
+    db=1
+    while s<n:
+        s=s+1+db
+        db=db+1
+    return db
+
+def feladat_9():
+    m=1
+    perc=0
+    k=1
+    while m<300:
+        m=m+m*(1/(1+k))
+        k=k+1
+        perc=perc+1
+    return perc
+
+def feladat_10():
+    try:
+        fajl=open("be.txt", mode="r")
+        max_hossz=-math.inf
+        for sor in fajl:
+            sor=sor.strip()
+            hossz=len(sor)
+            if sor[0].isupper() and hossz>max_hossz:
+                max_hossz=hossz
+        print(max_hossz)
+    except Exception as e:
+        print(e)
+
+def feladat_11():
+    try:
+        fajl=open("be.txt", mode="r")
+        max_hossz=-math.inf
+        abc="aábcdeéfghiíjklmnopqrstuúüűvwxyz"
+        for sor in fajl:
+            sor=sor.strip()
+            hossz=len(sor)
+            if sor[-1] not in abc and hossz>max_hossz:
+                max_hossz=hossz
+        print(max_hossz)
+    except Exception as e:
+        print(e)
+
+def feladat_12():
+    try:
+        fajl=open("be.txt", mode="r")
+        fajl2=open("ki.txt",mode="w")
+        for sor in fajl:
+            db = int(sor[-1])
+            egyforma=0
+            for i in range(1,len(sor)-1):
+                if sor[i]==sor[i+1]:
+                    egyforma=egyforma+1
+        if egyforma>=db:
+            fajl2.write("True")
+        else:
+            fajl2.write("False")
+    except Exception as e:
+        return e
+
+def feladat_15():
+    try:
+        fajl1=open("be.txt", mode="r")
+        fajl2=open("ki.txt", mode="w")
+        for sor in fajl1:
+            if sor!="\n":
+                fajl2.write(sor)
+
+    except Exception as e:
+        return (e)
+    fajl1.close()
+    fajl2.close()
 
 def main():
     print(feladat_1(8))
@@ -73,5 +147,10 @@ def main():
     print(feladat_3(513))
     feladat_4()
     feladat_5(1000)
+    print(feladat_8(51))
+    print(feladat_9())
+    feladat_10()
+    feladat_11()
+    feladat_12()
 if __name__ == '__main__':
     main()
